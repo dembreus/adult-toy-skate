b0603b12f940f43dbdaabc0aa3976f35d1e3075f
Dillon <dillon.embreus@gmail.com>
2018-12-14 00:24:59 -0700

    Added new library
    went a different direction with the bootstrap design

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import "../styles/App.css";
-import Routes from "../Routes";
+// import Routes from "../Routes";
+import NavBarPage from "./utils/navBarPage";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Routes />
-      </div>
-    );
+    return <div className="App">{/* <NavBarPage /> */}</div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./Components/App.jsx";
 import registerServiceWorker from "./registerServiceWorker";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
